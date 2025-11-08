7306de07648885c512a43ff400c406cc8d20b51e
saad123under <203528164+saad123under@users.noreply.github.com>
2025-11-08 18:38:00 +0100

    Create code_1.java
    first ftc java code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code_1.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class code_1 extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("hello","word");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
